2130f7fe23d648e58ac8fd6ea20850afd31f10fe
haiiro.simeji <haiiro.shimeji@gmail.com>
2012-06-19 20:34:50 +0900

    stub function.

--- template/mockObject.php
+++ template/mockObject.php
@@ -1,4 +1,22 @@
-class MockObject_<?php echo $tag ?> extends <?php echo $className ?> {
+class MockObject_<?php echo $tag ?> extends <?php echo $className ?> implements MockObject {
+
+	var $methods;
+	
+	protected function invokeMethod( $methodName, $args ) {
+	
+		$method = $this->methods[ $methodName ];
+		
+		if ( $method->isStub() ) {
+			return $method->invoke( $args );
+		} else {
+			return call_user_func_array(array($this, "parent::".$methodName), $args);
+		}
+	
+	}
+
+	function method( $methodName ) {
+		return $this->methods[ $methodName ];
+	}
 
 <?php
 	foreach ( $methods as $method ) {
@@ -6,7 +24,7 @@ class MockObject_<?php echo $tag ?> extends <?php echo $className ?> {
 		$accessor = ( $method->isProtected() ? 'protected' : 'public' );
 		
 		echo ( $method->isProtected() ? 'protected' : 'public' )
-			.' function '.$method->name.'() { return call_user_func_array(array($this, "parent::'.$method->name.'"), func_get_args()); }'."\n";
+			.' function '.$method->name.'() { return $this->invokeMethod( "'.$method->name.'", func_get_args() ); }'."\n";
 		
 	}
 ?>
