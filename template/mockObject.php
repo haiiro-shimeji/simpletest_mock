e83b2f8830a878d9cc87896a294b929f0d04c745
haiiro.simeji <haiiro.shimeji@gmail.com>
2012-06-19 17:58:38 +0900

    Add accessor keyword to function definitions in MockObject.

--- template/mockObject.php
+++ template/mockObject.php
@@ -2,7 +2,12 @@ class MockObject_<?php echo $tag ?> extends <?php echo $className ?> {
 
 <?php
 	foreach ( $methods as $method ) {
-		echo 'function '.$method->name.'() { return call_user_func_array(array($this, "parent::'.$method->name.'"), func_get_args()); }'."\n";
+		
+		$accessor = ( $method->isProtected() ? 'protected' : 'public' );
+		
+		echo ( $method->isProtected() ? 'protected' : 'public' )
+			.' function '.$method->name.'() { return call_user_func_array(array($this, "parent::'.$method->name.'"), func_get_args()); }'."\n";
+		
 	}
 ?>
 
